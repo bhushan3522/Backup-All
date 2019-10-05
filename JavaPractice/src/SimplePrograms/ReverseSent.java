37d425bb286bf3ea11ce37c2825f7f558c9e4b99
bhushan <bhushan3522@gmail.com>
2019-10-05 22:40:39 +0530

    Updated Programs for String Operations and String Words reversal

--- JavaPractice/src/SimplePrograms/ReverseSent.java
+++ JavaPractice/src/SimplePrograms/ReverseSent.java
@@ -1,26 +1,22 @@
 package SimplePrograms;
 
-import java.util.Scanner;
-
 public class ReverseSent {
 
 	public static void main(String[] args) {
 
-		System.out.println("Enter a sentance which needs to be reversed : ");
-		Scanner sc = new Scanner(System.in);
 		
-		String s = sc.next();
+		String s = "My name is Bhushan";
 		
 		String sArr[] = s.split(" ");
 		String ans = "";
-		for(int i=0 ; i < sArr.length; i++)
+		for(int i=sArr.length-1 ; i >=0 ; i--)
 		{
-			System.out.println(sArr[i]);
-			//ans = ans+sArr[i];
+			//System.out.print(sArr[i]);
+			ans = ans+sArr[i]+" ";
 			//System.out.println(ans);
 		}
 		
-		//System.out.println("New String is : "+ ans.substring(0, ans.length() - 1));
+		System.out.println("New String is : "+ ans);
 	}
 
 }
