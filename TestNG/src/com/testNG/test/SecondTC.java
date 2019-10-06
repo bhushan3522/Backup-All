8b66684412baa12021de75c3f07ad649d4e35339
bhushan <bhushan3522@gmail.com>
2019-10-06 18:02:42 +0530

    committed after adding program to test JDBC

--- TestNG/src/com/testNG/test/SecondTC.java
+++ TestNG/src/com/testNG/test/SecondTC.java
@@ -1,7 +1,5 @@
 package com.testNG.test;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
